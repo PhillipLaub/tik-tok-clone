cce153c20f8df17ba02e276a784afd37f6175a99
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-06-11 11:18:54 -0700

    pushing existing work

---
+++ src/components/Video.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div
+      className="
+        video"
+    >
+      <video src="https://www.tiktok.com/@billnye/video/6925937962970582277"></video>
+      {/* <VideoFooter/>  
+      <VideoSidebar/> */}
+    </div>
+  );
+}
+
+export default Video;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnL52uVQc4hJDN7fRez_ZuzLTYDHkzrEQ",
+  authDomain: "tik-tok-clone-af508.firebaseapp.com",
+  projectId: "tik-tok-clone-af508",
+  storageBucket: "tik-tok-clone-af508.appspot.com",
+  messagingSenderId: "744856455493",
+  appId: "1:744856455493:web:e3c90c451676a4ffe6d62c",
+  measurementId: "G-7GJJ4FXK3F",
+};
